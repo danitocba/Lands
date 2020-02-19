245ddc2a6af97b147d2b507319619760e0979cd7
danitocba <danielalaue@gmail.com>
2020-02-18 22:38:29 -0300

    Danielo
    Danielo

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace Lands
+{
+    using System;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    using Views;
+
+    public partial class App : Application
+    {
+        #region Constructor
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage (new LoginPage());
+        }
+        #endregion
+
+
+        #region Mehods
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        #endregion
+    }
+}
